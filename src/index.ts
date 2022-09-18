743979e3f6869f105c72a7d058e849c6eeca9471
Jakob Offersen <jakoboffersen@gmail.com>
2022-09-18 10:59:08 +0200

    add asyncErrorCatcher

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const hello = () => {
-  return "world"
-}
+import { RequestHandler, Request, Response, NextFunction } from "express"
+
+// forwards any thrown error to the middleware.
+export const asyncErrorCatcher = <
+  P,
+  ResBody,
+  ReqBody,
+  ReqQuery,
+  Locals extends Record<string, any>>
+  (fn: RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>) => (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction) => Promise.resolve(fn(req, res, next)).catch(next)
